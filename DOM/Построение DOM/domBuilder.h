15555569fe9609cafee96b997bc2b1362bb6f2dd
PKislov <andrey251209@k66.ru>
2015-09-30 21:41:47 +0600

    Update domBuilder.h

--- DOM/Построение DOM/domBuilder.h
+++ DOM/Построение DOM/domBuilder.h
@@ -1,56 +1,53 @@
 #ifndef DOMBUILDER_INCLUDED
 #define DOMBUILDER_INCLUDED
-#include <string>
-#include <vector>
-
-class Dom
+#include #include class Dom
 {
-    // òèïû óçëîâ
-    const std::string document="document"; // êîðåíü äåðåâà, óçåë ãåíåðèðóåòñÿ êîíñòðóêòîðîì, íóæåí ôîðìàëüíî
-    const std::string title="title"; // çàãîëîâîê äîêóìåíòà
-    const std::string toc="toc"; // ñîäåðæàíèå (îãëàâëåíèå)
-    const std::string section1="section1";
-    const std::string section2="section2";
-    const std::string section3="section3";
-    const std::string section4="section4";
-    const std::string text="text"; // ïðîèçâîëüíûé òåêñò
-    const std::string image="image";
-    const std::string nothing="nothing"; // ïóñòîé óçåë
+// типы узлов
+const std::string document="document"; // корень дерева, узел генерируется конструктором, нужен формально
+const std::string title="title"; // заголовок документа
+const std::string toc="toc"; // содержание (оглавление)
+const std::string section1="section1";
+const std::string section2="section2";
+const std::string section3="section3";
+const std::string section4="section4";
+const std::string text="text"; // произвольный текст
+const std::string image="image";
+const std::string nothing="nothing"; // пустой узел
 
-    struct node
-    {
-        node *parent; // óêàçàòåëü íà ðîäèòåëÿ óçëà
-        std::vector <node*>children; // ìàññèâ óêàçàòåëåé íà ïîòîìêîâ óçëà
-        std::string id; // òèï óçëà (òåêñò, ðèñóíîê, çàãîëîâîê è ò.ä.)
-        std::vector <std::string>value; // ìàññèâ çíà÷åíèé (àòòðèáóòîâ) óçëà (ó ðàçíûõ òèïîâ ðàçíûé íàáîð)
-    };
+struct node
+{
+node *parent; // указатель на родителя узла
+std::vector children; // массив указателей на потомков узла
+std::string id; // тип узла (текст, рисунок, заголовок и т.д.)
+std::vector value; // массив значений (аттрибутов) узла (у разных типов разный набор)
+};
 
-    node *root; // êîðåíü äåðåâà (âñåãäà id ðàâíî "document")
-    node *temp; // óêàçàòåëü íà óçåë, ñ êîòîðûì â ïîñëåäíèé ðàç ïðîâîäèëàñü îïåðàöèÿ
+node *root; // корень дерева (всегда id равно "document")
+node *temp; // указатель на узел, с которым в последний раз проводилась операция
 
-	struct node* addChild(struct node *p); // äîáàâèòü ïîòîìêà óçëó p è âîçâðàòèòü ïîòîìêà
-	struct node* addBro(struct node *p); // äîáàâèòü â äåðåâî óçåë îäèíàêîâîãî ópîâíÿ ñ p è âîçâðàòèòü åãî
-	void delElem(struct node *p); // óäàëÿåò âñþ ÷àñòü äåðåâà ïîñëå óçëà p. Èñï. â äåñòðóêòîðå.
-	inline bool isSection (const struct node *p) const; // âîçâðàùàåò true, åñëè óçåë ð - çàãîëîâîê
+struct node* addChild(struct node *p); // добавить потомка узлу p и возвратить потомка
+struct node* addBro(struct node *p); // добавить в дерево узел одинакового уpовня с p и возвратить его
+void delElem(struct node *p); // удаляет всю часть дерева после узла p. Исп. в деструкторе.
+inline bool isSection (const struct node *p) const; // возвращает true, если узел р - заголовок
 
- public:
+public:
 
-    Dom();
-    ~Dom();
+Dom();
+~Dom();
 
-    // äîáàâëåíèå ýëåìåíòîâ â äåðåâî (ìåòîäû äëÿ ëåêñåðà):
-    void addText(FILE *f, const char *name); // Òåêñò
-    void addToc(); // Ñîäåðæàíèå
-    void addTitle(const char *s); // Íàçâàíèå äîêóìåíòà
-    void addImageId(const char *s); // ðèñóíîê ñ id
-    void addImageRef(const char *s); // ðèñóíîê ñ ññûëêîé
-    void addSection1(const char *s); // çàãîëîâîê óðîâíÿ 1
-    void addSection2(const char *s); // çàãîëîâîê óðîâíÿ 2
-    void addSection3(const char *s); // çàãîëîâîê óðîâíÿ 3
-    void addSection4(const char *s); // çàãîëîâîê óðîâíÿ 4
-    
+// добавление элементов в дерево (методы для лексера):
+void addText(FILE *f, const char *name); // Текст
+void addToc(); // Содержание
+void addTitle(const char *s); // Название документа
+void addImageId(const char *s); // рисунок с id
+void addImageRef(const char *s); // рисунок с ссылкой
+void addSection1(const char *s); // заголовок уровня 1
+void addSection2(const char *s); // заголовок уровня 2
+void addSection3(const char *s); // заголовок уровня 3
+void addSection4(const char *s); // заголовок уровня 4
+
 private:
-    Dom (const Dom &d) {}
-    Dom& operator= (const Dom &d) { return *this;}
+Dom (const Dom &d) {}
+Dom& operator= (const Dom &d) { return *this;}
 };
 #endif // DOMBUILDER_INCLUDED
