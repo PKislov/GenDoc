0c6b60991fe012f36d221a6821eabc4274eac12a
PKislov <andrey251209@k66.ru>
2015-10-14 09:58:31 +0300

    Update GenDoc.h

--- DOM/Построение DOM/GenDoc.h
+++ DOM/Построение DOM/GenDoc.h
@@ -23,13 +23,21 @@ private:
     void makeBuff (const decltype(root) p, std::string &buf) const;
     // заполняет в шаблоне (в строке buf) заголовок документа (title, в строке t)
     void writeTitle (const std::string &t, std::string &buf) const;
+    void writeSection1 (const std::string &t, std::string &buf) const;
+    void writeSection2 (const std::string &t, std::string &buf) const;
+    void writeSection3 (const std::string &t, std::string &buf) const;
+    void writeSection4 (const std::string &t, std::string &buf) const;
     // метод возвращает индекс перед выражением "\end{document}" в строке buf, используется для определения места вставки текста
     unsigned whereInsertText (const std::string &buf) const;
     // записывает текст из t в buf по индексу, вычисленному методом whereInsertText
-    void writeText (const std::string &t, std::string &buf) const;
+    inline void writeText (const std::string &t, std::string &buf) const;
     void writeImage (const decltype(root) p, std::string &buf) const;
-    // ищет в строке s служебные символы языка LaTex и заменяет их командами LaTex, позволяющими печатать эти символы, например все найденные символы # заменит на \# (применяется в методе writeText и др.
+    // делает то же свмое, что headerToLaTex, только еще дублирует переходы на новую строку
     const std::string& strToLaTex (std::string &s) const;
+    // ищет в строке s служебные символы языка LaTex и заменяет их командами LaTex, позволяющими печатать эти символы, например все найденные символы # заменит на \# (применяется в методе writeTitle, writeSection и др.)
+    const std::string& headerToLaTex (std::string &s) const;
+    // записывает текст из t в buf по индексу, вычисленному методом whereInsertText, преобразует текст методом strToLaTex
+    inline void writeTextLatex (const std::string &t, std::string &buf) const;
 
 };
 
