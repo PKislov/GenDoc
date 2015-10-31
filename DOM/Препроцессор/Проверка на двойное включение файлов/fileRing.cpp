d1d28382d70c5a0a1bf55e3610e51ffbd44f7430
Pavel <andrey251209@k66.ru>
2015-10-31 22:46:09 +0600

    Update fileRing.cpp

--- DOM/Препроцессор/Проверка на двойное включение файлов/fileRing.cpp
+++ DOM/Препроцессор/Проверка на двойное включение файлов/fileRing.cpp
@@ -4,10 +4,10 @@
 FileTree::FileTree(const char *n) // в параметрах - имя "главного" файла
 {
 	root = new struct node;
-    root->parent=NULL;
-    root->name=n;
+    	root->parent = NULL;
+    	root->name = n;
 	temp = root;
-	root->countF=0;
+	root->countF = 0;
 }
 FileTree::~FileTree()
 {
@@ -20,30 +20,30 @@ struct FileTree::node* FileTree::addChild(struct node *p)
 {
     if (!p) return NULL;
     p->children.push_back(new struct node);
-    p->children.back()->parent=p;
+    p->children.back()->parent = p;
     return p->children.back();
 }
 // добавление элементов в дерево.
 bool FileTree::addElem(const char *name) // В параметрах - имя файла
 {
-	temp=addChild(temp);
-	temp->countF=0;
+	temp = addChild(temp);
+	temp->countF = 0;
 	temp->name = name;
 
-    auto ptemp=temp->parent;
+    auto ptemp = temp->parent;
     while (ptemp) // поиск закольцованностей
     {
         if (ptemp->name == temp->name) return false;
-        ptemp=ptemp->parent;
+        ptemp = ptemp->parent;
     }
     return true;
 }
 void FileTree::delElem(struct node *p) // удаление из памяти элементов дерева после узла p
 {
 	if (!p || !p->children.size()) return;
-	for (int i=0; i<p->children.size(); ++i)
+	for (decltype(p->children.size()) i=0; i < p->children.size(); ++i)
 		if (p->children[i]->children.size()) delElem (p->children[i]);
-	for (int i=0; i<p->children.size(); ++i)
+	for (decltype(p->children.size()) i=0; i < p->children.size(); ++i)
 	{
 		//puts(p->children[i]->name.c_str());
 		delete p->children[i];
