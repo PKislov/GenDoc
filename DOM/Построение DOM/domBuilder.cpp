de0c195922fca0ea0d4fbda6acb30955e33333ed
Pavel <andrey251209@k66.ru>
2015-11-05 14:31:12 +0300

    Update domBuilder.cpp

--- DOM/Построение DOM/domBuilder.cpp
+++ DOM/Построение DOM/domBuilder.cpp
@@ -38,14 +38,6 @@ struct Dom::node* Dom::addChild(struct node *p)
     p->children.back()->parent = p;
     return p->children.back();
 }
-// добавить узел одинакового уpовня с p и возвратить его
-/*struct Dom::node* Dom::addBro(struct node *p)
-{
-    if (!p || !p->parent) return NULL;
-    p->parent->children.push_back(new struct node);
-    p->parent->children.back()->parent = p;
-    return p->parent->children.back();
-}*/
 
 // возвращает true, если узел р - заголовок
 bool Dom::isSection (const struct node * p) const
@@ -53,21 +45,12 @@ bool Dom::isSection (const struct node * p) const
     return p->id == section1 || p->id == section2 || p->id == section3 || p->id == section4;
 }
 
-/*void Dom::addTitle(const char *s) // Название документа
+bool Dom::isSectionBegin (const struct node * p) const
 {
-    temp = addChild(root);
-    temp->id = title;
-    temp->value.push_back("");
-    s += std::strlen("@title ");
-    while(*s != '@')
-    {
-        temp->value.back().push_back(*s);
-        ++s;
-    }
-    temp = temp->parent;
-}*/
+    return p->id == section1Begin || p->id == section2Begin || p->id == section3Begin || p->id == section4Begin;
+}
 
-void Dom::addTitleBegin() // начало команды @title
+void Dom::addTitleBegin(const char *s) // начало команды @title
 {
     static bool f = false;
     if (f)
@@ -78,6 +61,10 @@ void Dom::addTitleBegin() // начало команды @title
     f = true;
     temp = addChild(root);
     temp->id = titleBegin;
+    // под название документа, пока для поисковика ошибок записать текст команды в оригинальном регистре
+    temp->value.push_back(s);
+    // замена в строке последовательности символов "\\a" на "\""
+    SeqSymbContrReplace(temp->value.back());
     temp = temp->parent;
 }
 void Dom::addTitle() // окончание команды - @end title
@@ -95,7 +82,7 @@ void Dom::addTitle() // окончание команды - @end title
         if (root->children[i]->id == titleBegin) // нашли начало команды
         {
             root->children[i]->id = title;
-            root->children[i]->value.push_back(""); // под название документа
+            root->children[i]->value.back() = "";
             // если пустой заголовок
             if(i+1 == root->children.size())
                 return;
@@ -115,6 +102,11 @@ void Dom::addTitle() // окончание команды - @end title
                 root->children.erase(root->children.begin()+i1);
                 i1 = i;
             }
+            if (root->children[i]->value.back().find("\n\n", 0) != std::string::npos)
+            {
+                puts("В команде описания команды \"@title ... @end title\" нельзя использовать двойные переходы на новую строку!");
+                exit(1);
+            }
             return;
         }
     }
@@ -122,21 +114,22 @@ void Dom::addTitle() // окончание команды - @end title
     exit(1);
 }
 
-void Dom::addSection1(const char *s) // заголовок уровня 1
+void Dom::addSection1Begin(const char *s) // заголовок уровня 1
 {
     temp = addChild(root);
-    temp->id = section1;
-    temp->value.push_back("");
-    s += std::strlen("@section1 ");
-	while(*s) temp->value.back().push_back(*s), ++s;
-	delNewLineEndStr(temp->value.back());
+    temp->id = section1Begin;
+    temp->value.push_back("text"); // параметр
+    temp->value.push_back(""); // название заголовка
+    temp->value.push_back(""); // без параметра ссылки
+    temp->value.push_back(""); // значение ссылки
+    // заголовок занесется в методе addText
 }
 
-void Dom::addSection2(const char *s) // заголовок уровня 2
+void Dom::addSection2Begin(const char *s) // заголовок уровня 2
 {
     decltype(temp) plast = NULL; // проверка
     for (decltype(root->children.size()) i=0; i < root->children.size(); ++i)
-        if (root->children[i]->id == section1)
+        if (root->children[i]->id == section1 || root->children[i]->id == section1Begin)
             plast = root->children[i];
     if (!plast)
     {
@@ -145,21 +138,21 @@ void Dom::addSection2(const char *s) // заголовок уровня 2
     }
 
     temp = addChild(plast);
-    temp->id = section2;
-    temp->value.push_back("");
-    s += std::strlen("@section2 ");
-	while(*s) temp->value.back().push_back(*s), ++s;
-	delNewLineEndStr( temp->value.back());
+    temp->id = section2Begin;
+    temp->value.push_back("text"); // параметр
+    temp->value.push_back(""); // название заголовка
+    temp->value.push_back(""); // без параметра ссылки
+    temp->value.push_back(""); // значение ссылки
 }
 
-void Dom::addSection3(const char *s) // заголовок уровня 3
+void Dom::addSection3Begin(const char *s) // заголовок уровня 3
 {
-    decltype(temp) plast=NULL; // проверка
+    decltype(temp) plast = NULL; // проверка
     for (decltype(root->children.size()) i=0; i < root->children.size(); ++i)
-        if (root->children[i]->id == section1)
+        if (root->children[i]->id == section1 || root->children[i]->id == section1Begin)
             for (decltype(i) j=0; j < root->children[i]->children.size(); ++j)
             {
-                if (root->children[i]->children[j]->id == section2)
+                if (root->children[i]->children[j]->id == section2 || root->children[i]->children[j]->id == section2Begin)
                     plast = root->children[i]->children[j];
             }
 
@@ -170,22 +163,22 @@ void Dom::addSection3(const char *s) // заголовок уровня 3
     }
 
     temp = addChild(plast);
-    temp->id = section3;
-    temp->value.push_back("");
-    s += std::strlen("@section3 ");
-	while(*s) temp->value.back().push_back(*s), ++s;
-	delNewLineEndStr( temp->value.back());
+    temp->id = section3Begin;
+    temp->value.push_back("text"); // параметр
+    temp->value.push_back(""); // название заголовка
+    temp->value.push_back(""); // без параметра ссылки
+    temp->value.push_back(""); // значение ссылки
 }
 
-void Dom::addSection4(const char *s) // заголовок уровня 4
+void Dom::addSection4Begin(const char *s) // заголовок уровня 4
 {
-    decltype(temp) plast=NULL; // проверка
+    decltype(temp) plast = NULL; // проверка
     for (decltype(root->children.size()) i=0; i < root->children.size(); ++i)
-        if (root->children[i]->id == section1)
+        if (root->children[i]->id == section1 || root->children[i]->id == section1Begin)
             for (decltype(i) j=0; j < root->children[i]->children.size(); ++j)
-                if (root->children[i]->children[j]->id == section2)
+                if (root->children[i]->children[j]->id == section2 || root->children[i]->children[j]->id == section2Begin)
                     for (decltype(i) jj=0; jj < root->children[i]->children[j]->children.size(); ++jj)
-                        if (root->children[i]->children[j]->children[jj]->id == section3)
+                        if (root->children[i]->children[j]->children[jj]->id == section3 || root->children[i]->children[j]->children[jj]->id == section3Begin)
                             plast = root->children[i]->children[j]->children[jj];
 
     if (!plast)
@@ -195,11 +188,73 @@ void Dom::addSection4(const char *s) // заголовок уровня 4
     }
 
     temp = addChild(plast);
-    temp->id = section4;
-    temp->value.push_back("");
-    s += std::strlen("@section4 ");
-	while(*s) temp->value.back().push_back(*s), ++s;
-	delNewLineEndStr( temp->value.back());
+    temp->id = section4Begin;
+    temp->value.push_back("text"); // параметр
+    temp->value.push_back(""); // название заголовка
+    temp->value.push_back(""); // без параметра ссылки
+    temp->value.push_back(""); // значение ссылки
+}
+
+// заголовок уровня sec (команда вида @section1{text:"..."}
+void Dom::addSection_1Param(const char *s, const std::string &sec, decltype(root->children.size()) n)
+{
+    const char * const sbegin = s; // переменная для поиска ошибок
+
+    // массив указателей на методы addSectionBegin
+    void (Dom::*pfaddSecBeg[])(const char*) = {&Dom::addSection1Begin, &Dom::addSection2Begin, &Dom::addSection3Begin, &Dom::addSection4Begin};
+    // вызов нужного метода, индекс в массиве указателей берется из строки sec и преобразуется из char в int (ASCII код '1' в 1)
+    (this->*(pfaddSecBeg[sec[section1.size()-1]-'0'-1]))(s);
+    temp->id = sec; // указать уровень заголовка
+    while(*s != '\"') ++s; ++s; // дойти до начала text
+    while(*s != '\"') temp->value[n].push_back(*s), ++s; ++s; // записать заголовок
+    // замена в строке последовательности символов "\\a" на "\""
+    SeqSymbContrReplace(temp->value[n]);
+    // если заголовок содержит двойные переходы на новую строку, то TexLive это
+    // примет за ошибку
+    if (temp->value[n].find("\n\n", 0) != std::string::npos)
+    {
+        printf("В команде описания заголовка \"%s\" нельзя использовать двойные переходы на новую строку!\n", sbegin);
+        exit(1);
+    }
+}
+
+// команда вида @section{text:"..."; id:"..."} (n=3) или @section1{id:"..."; text:"..."} (n=1) - вид определяется параметром n
+void Dom::addSection_2Param(const char *s, const std::string &sec, decltype(root->children.size()) n)
+{
+    const char * const sbegin = s; // переменная для поиска ошибок
+    // text ... id ... // в value[n] записать ссылку, n=3 или =1
+    if (n == 3)
+        addSection_1Param(s, sec);
+    else // n == 1
+        addSection_1Param(s, sec, 3);
+
+    while(*s != '\"') ++s; ++s; // дойти до начала text
+    while(*s != '\"') ++s; ++s; // дойти до конца text
+    while(*s != '\"') ++s; ++s; // дойти до начала id
+    while(*s != '\"') temp->value[n].push_back(*s), ++s; ++s; // записать заголовок
+    // замена в строке последовательности символов "\\a" на "\""
+    SeqSymbContrReplace(temp->value[n]);
+    // если заголовок содержит двойные переходы на новую строку, то TexLive это
+    // примет за ошибку
+    if (temp->value[n].find("\n\n", 0) != std::string::npos)
+    {
+        printf("В команде описания заголовка \"%s\" нельзя использовать двойные переходы на новую строку!\n", sbegin);
+        exit(1);
+    }
+    temp->value[2] = "id";
+    if (!temp->value[3].size())
+    {
+        printf("В команде описания заголовка \"%s\" нельзя использовать пустое значение ссылки!\n", sbegin);
+        exit(1);
+    }
+
+    temp->value.push_back("yylex"); // параметр yylex
+    temp->value.push_back(sbegin); // значение yylex - строка команды от лексера (нужна для поиска повторов ссылок)
+    // замена в строке последовательности символов "\\a" на "\""
+    SeqSymbContrReplace(temp->value.back());
+
+    showDuplicateIdInDom(temp);
+
 }
 
 void Dom::addToc() // Содержание
@@ -207,7 +262,6 @@ void Dom::addToc() // Содержание
     temp = addChild(root);
 	temp->id = toc;
 	temp = temp->parent;
-	ftoc = true;
 }
 
 void Dom::addImageRef(const char *s,  decltype(root->children.size()) n1, decltype(n1) n2) // рисунок с ref
@@ -218,11 +272,23 @@ void Dom::addImageRef(const char *s,  decltype(root->children.size()) n1, declty
     temp->value.push_back(""); // подпись рисунка
     temp->value.push_back("ref");
     temp->value.push_back(""); // значение ref
+    temp->value.push_back(""); // параметр id
+    temp->value.push_back(""); // значение id
     while(*s != '\"') ++s; ++s; // дойти до начала подписи
     while(*s != '\"') temp->value[n1].push_back(*s), ++s; ++s; // записать подпись
+    // замена в строке последовательности символов "\\a" на "\""
+    SeqSymbContrReplace(temp->value[n1]);
 	while(*s != '\"') ++s; ++s;
 	while (isspace(*s)) ++s;
 	while(*s != '\"') temp->value[n2].push_back(*s), ++s;
+	// замена в строке последовательности символов "\\a" на "\""
+    SeqSymbContrReplace(temp->value[n2]);
+	// поиск в параметрах двойных переходов на новую строку
+	if ((temp->value[1].find("\n\n", 0) != std::string::npos) || (temp->value[3].find("\n\n", 0) != std::string::npos))
+    {
+        puts("В команде описания рисунка нельзя использовать двойные переходы на новую строку!");
+        exit(1);
+    }
 	temp = temp->parent;
 }
 void Dom::addImageRef(const char *s) // рисунок с ref
@@ -233,8 +299,12 @@ void Dom::addImageRef(const char *s) // рисунок с ref
     temp->value.push_back(""); // подпись рисунка
     temp->value.push_back("ref");
     temp->value.push_back(""); // значение ref
+    temp->value.push_back(""); // параметр id
+    temp->value.push_back(""); // значение id
     while(*s != '\"') ++s; ++s; // дойти до начала ref
     while(*s != '\"') temp->value[3].push_back(*s), ++s; ++s; // записать ref
+    // замена в строке последовательности символов "\\a" на "\""
+    SeqSymbContrReplace(temp->value[3]);
 	temp = temp->parent;
 }
 
@@ -262,22 +332,154 @@ void Dom::addText(FILE *f, const char *name) // Текст
     }
     fclose(f);
     remove(name);
+    // замена в строке последовательности символов "\\a" на "\""
+    SeqSymbContrReplace(temp->value.back());
     if (!(f = fopen(name, "wt")))
     {
         printf ("Не удалось открыть временный файл \"%s\"\n", name);
 		exit (1);
     }
+    // если родитель узла - начало команды section, в значение родителя
+    // записать в значение родителя  текст из текущего узла до символа новой строки,
+    // удалить это текст из текста тек. узла и поменять идентификатор родителя sectionBegin
+    // на section
+    if(isSectionBegin(temp->parent))
+    {
+        int i=0;
+        // поиск индекса окончания заголовка - до символа новой строки
+        while ((i < temp->value.back().size()) && (temp->value.back()[i] != '\n')) ++i;
+        // запись текста заголовка
+        temp->parent->value[1].insert(0, temp->value.back(), 0 , i);
+        temp->value.back().erase(0, i); // удаление текста заголовка из текста тек. узла
+        // меняем id заголовка с sectionBegin на section
+        sectionBegToSection(temp->parent->id);
+    }
     temp = temp->parent;
 }
 
-void Dom::delNewLineEndStr (std::string &s) const
+// меняет строку id с sectionBegin на section (используется в построении узлов заголовков)
+const std::string& Dom::sectionBegToSection (std::string &id) const
 {
-    // удалить переходы на новую строку в конце строки
-    for(decltype(s.size()) i=s.size()-1; i >= 0; --i)
+    id.erase(section1.size(), section1Begin.size() - section1.size());
+    return id;
+}
+
+// ищет в дереве узел содержащий ссылку id и возвращает его, или NULL если узел не найден (поиск повторов ссылок)
+const struct Dom::node* Dom::findIdInDom (const struct node *p, const std::string &id) const
+{
+    if (!p || !p->children.size()) return NULL; // если пустрое дерево
+
+    for (decltype(p->children.size()) i=0; i < p->children.size(); ++i) // цикл прохода дерева
     {
-        if(s[i] == 10 || s[i] == 13 || s[i] == '\n')
-            s.erase(i);
-        else
-            break;
+        // если узел - заголовок или рисунок и содержит ссылку id
+        auto indexId = getIndexId(p->children[i]);
+        if (indexId && (p->children[i]->value[indexId] == id))
+            return p->children[i];
+
+        if (p->children[i]->children.size())
+        {
+            const struct node* result = findIdInDom (p->children[i], id);
+            if (result) return result; // ссылка найдена
+        }
     }
+    return NULL;
+}
+
+// ищет в DOM ссылку, содержащуюся в узле р (метод findIdInDom)
+// и выводит сообщение о результате. Возвращает true если ссылка найдена.
+bool Dom::showDuplicateIdInDom (const struct node *p) const
+{
+    auto indexIdp = getIndexId(p); // индекс ссылки в векторе value узла р
+    if (!indexIdp)
+        return false;
+    const struct node *pfind = findIdInDom(root, p->value[indexIdp]);
+    // если такая ссылка уже есть в DOM (кроме узла p)
+    if (pfind && (pfind != p))
+    {
+        // индекс выражения yytext в найденном узле (зависит от типа узла)
+        auto indexYYtextRes = getIndexYytext(pfind);
+        /*if (!indexYYtextRes)
+            return false;*/
+
+        // индекс выражения yytext в узле p (зависит от типа узла)
+        auto indexYYtextP = getIndexYytext(p);
+       /* if (!indexYYtextP)
+            return false;*/
+
+        printf ("Ссылка \"%s\", указанная в выражении \"%s\", уже была указана в выражении \"%s\"!\n", p->value[indexIdp].c_str(), p->value[indexYYtextP].c_str(), pfind->value[indexYYtextRes].c_str());
+        exit(1);
+        return true;
+    }
+    return false;
+}
+
+// возвращает индекс значения ссылки (id) в векторе value узла р,
+// возвращает 0 если узел не содержит ссылку
+std::string::size_type Dom::getIndexId(const struct node *p) const
+{
+    if (isSection(p))
+        return 3;
+    else
+        if (p->id == image)
+            return 5;
+    else
+        return 0;
+}
+
+// возвращает индекс значения yytext в векторе value узла р,
+// возвращает 0 если узел не содержит yytext
+std::string::size_type Dom::getIndexYytext(const struct node *p) const
+{
+    if (isSection(p))
+        return 5;
+    else
+        if ((p->id == image) && p->value.size() >= 8) // у рисунка полей под yytext может не быть
+            return 7;
+    else
+        return 0;
+}
+
+// ссылка - команда &... {id:"..."}, параметр res означает, на что указывает ссылка (заголовок, рисунок, ...)
+void Dom::addId(const char *s, const std::string &res)
+{
+    temp = addChild(temp);
+    temp->id = idRef;
+    temp->value.push_back("id");
+    temp->value.push_back(""); // значение ссылки
+    temp->value.push_back("yylex"); // параметр yylex
+    temp->value.push_back(s);
+    // замена в строке последовательности символов "\\a" на "\""
+    SeqSymbContrReplace(temp->value.back());
+    temp->value.push_back("res"); // на какой ресурс указывает (рисунок, таблица, ...)
+    temp->value.push_back(res);
+    // замена в строке последовательности символов "\\a" на "\""
+    SeqSymbContrReplace(temp->value.back());
+    while(*s != '\"') ++s; ++s; // дойти до начала ref
+    while(*s != '\"') temp->value[1].push_back(*s), ++s; ++s; // записать id
+    // замена в строке последовательности символов "\\a" на "\""
+    SeqSymbContrReplace(temp->value[1]);
+    if (temp->value[1].find("\n\n", 0) != std::string::npos)
+    {
+        printf("В команде описания ссылки \"%s\" нельзя использовать двойные переходы на новую строку!\n", s);
+        exit(1);
+    }
+    if (!temp->value[1].size())
+    {
+        printf("В команде описания ссылки \"%s\" нельзя использовать пустое значение ссылки!\n", s);
+        exit(1);
+    }
+	temp = temp->parent;
+}
+
+// замена в строке последовательности символов "\\a" на "\""
+const std::string& Dom::SeqSymbContrReplace (std::string &s) const
+{
+    if (replaceSeqSymbContr && s.size() >= 2)
+    {
+        for(decltype(s.size()) i=0; i < s.size()-1; ++i)
+            if (s[i] == '\\' && s[i+1] == '\a')
+                s[i+1] = '\"', ++i;
+    }
+    return s;
+
 }
